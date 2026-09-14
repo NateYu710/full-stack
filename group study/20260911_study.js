7565f17886bdc12a9e6dddde641fcadf89910265
NateYu710 <710ynh@gmail.com>
2026-09-14 18:02:48 +0900

    20260914

--- group study/20260911_study.js
+++ group study/20260911_study.js
@@ -1,43 +1,104 @@
-const Members = [
-  { name: '홍길동', point: 1200, level: 'VIP' },
-  { name: '유관순', point: 500, level: '일반' },
-  { name: '이순신', point: 2000, level: 'VIP' },
-  { name: '김철수', point: 0, level: '일반' },
-];
+/*
+Member(name, options) 생성자 함수를 작성하세요.
 
-/**
- * VIP 회원만 새로운 배열로 만들기
-포인트가 1000 이상인 회원들의 이름만 배열로 만들기
-포인트가 0인 회원 한 명 찾기
-포인트가 0인 회원이 한 명이라도 있는지 확인하기
-모든 회원의 포인트가 0 이상인지 확인하기
-포인트가 높은 순서대로 정렬하기
-전체 회원의 포인트 합계 구하기
- */
+프로퍼티
+- name
+- options
 
-const VipMember = Members.filter((member) => member.level === 'VIP');
-console.log(VipMember);
+Member.prototype에 다음 메서드를 작성하세요.
 
-const BigPoint = Members.filter((member) => member.point >= '1000').map(
-  (member) => {
-    return member.name;
-  },
-);
-console.log(BigPoint);
+1. getPoint()
+   - options.point를 반환
+   - 없으면 0 반환
 
-const ZeroPoint = Members.find((member) => member.point === 0);
-console.log(ZeroPoint);
+2. addPoint(point)
+   - 기존 포인트에 point를 더합니다.
 
-const ZeroPoint1 = Members.some((member) => member.point === 0);
-console.log(ZeroPoint1);
+3. usePoint(point)
+   - 보유 포인트보다 사용하려는 포인트가 크면
+     '포인트가 부족합니다.' 반환
+   - 가능하면 포인트를 차감하고
+     '포인트를 사용했습니다.' 반환
 
-const ZeroPoint2 = Members.every((member) => member.point >= 0);
-console.log(ZeroPoint2);
+회원 생성
 
-const OrderMember = Members.sort((a, b) => b.point - a.point);
-console.log(OrderMember);
+const member1 = new Member('홍길동', { point: 1000 });
+const member2 = new Member('유관순');
 
-const SumPoint = Members.reduce((sum, current) => {
-  return sum + current.point;
-}, 0);
-console.log(SumPoint);
+실행
+
+console.log(member1.getPoint());
+member1.addPoint(500);
+console.log(member1.getPoint());
+
+console.log(member1.usePoint(1200));
+console.log(member1.getPoint());
+
+console.log(member2.getPoint());
+member2.addPoint(300);
+console.log(member2.getPoint());
+
+예상 출력
+
+1000
+1500
+포인트를 사용했습니다.
+300
+0
+300
+*/
+
+function Member(name, options) {
+    this.name = name
+    this.options = options; 
+};
+
+
+Member.prototype.getPoint =  function(){
+    return this.options ?. point ?? 0
+};
+
+    /* ex:) member2.options = {
+    point: 0 가 생성되는 건 아님
+    */
+
+Member.prototype.addPoint = function(point){
+    if (this.options ?. point == undefined ) { //객체가 없으면 undefined를 반환
+        this.options = {} //빈객체 생성
+        this.options.point = point
+        return this.options.point
+    } else {
+        this.options.point += point
+        return this.options.point;
+    };
+    
+};
+Member.prototype.usePoint = function(point){
+    if(this.options ?. point === undefined) {
+        this.options = {}
+        this.options.point = 0
+        return '포인트가 없습니다'
+    } else if (this.options.point < point) {
+        return '포인트가 부족합니다'
+    } else {this.options.point -= point
+        return '포인트를 사용했습니다' };
+};
+
+const member1 = new Member('홍길동', { point: 1000 });
+const member2 = new Member('유관순');
+
+console.log(member2.getPoint());
+console.log(member2.usePoint(100));
+
+console.log(member1.getPoint()); //1000
+member1.addPoint(500); 
+console.log(member1.getPoint()); //1500
+
+console.log(member1.usePoint(1200));
+console.log(member1.getPoint());//300
+
+console.log(member2.getPoint());
+member2.addPoint(300);
+console.log(member2.getPoint());
+
+
